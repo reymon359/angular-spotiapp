f3ccac76d17dbe0da6fdf4e14e0920fb4871f208
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-04-16 21:38:06 +0200

    centralizing spotify requests

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -11,20 +11,21 @@ export class SpotifyService {
   constructor(private http: HttpClient) {
   }
 
-  getNewReleases() {
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQBGXTaKM-ZPfeAr_u3PU330bMmo6peGlzKnYdnaeglToVEPPHW1gUOfl-69E1ppddeA7777NtSiHJ2TdpY'
     });
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
-      .pipe(map(data => data['albums'].items));
+    return this.http.get(url, { headers });
+  }
+
+  getNewReleases() {
+    return this.getQuery('browse/new-releases?limit=20')
+       .pipe(map(data => data['albums'].items));
   }
 
   getArtist(term: string) {
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQBGXTaKM-ZPfeAr_u3PU330bMmo6peGlzKnYdnaeglToVEPPHW1gUOfl-69E1ppddeA7777NtSiHJ2TdpY'
-    });
-    return this.http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers })
+    return this.getQuery(`search?q=${term}&type=artist&limit=15`)
       .pipe(map(data => data['artists'].items));
-
   }
 }
