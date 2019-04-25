059693c2ed8e5e2ffe95dd775640befac998db52
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-04-25 21:45:47 +0200

    artist loading

--- src/app/components/artist/artist.component.ts
+++ src/app/components/artist/artist.component.ts
@@ -12,16 +12,24 @@ export class ArtistComponent {
 
   artist: any = {};
 
+  loadingArtist: boolean;
+
   constructor(private router: ActivatedRoute, private spotifyService: SpotifyService) {
+    this.loadingArtist = true;
     this.router.params.subscribe(params => {
       this.getArtist(params['id']);
     });
   }
 
   getArtist(id: string) {
+    this.loadingArtist = true;
+
     this.spotifyService.getArtist(id).subscribe(artist => {
       console.log(artist);
+
       this.artist = artist;
+      this.loadingArtist = false;
+
     });
   }
 
